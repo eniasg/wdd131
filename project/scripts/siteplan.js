68fba7f5f4153f7a3f308012afea7c0602d1a370
eniasg <122409346+eniasg@users.noreply.github.com>
2025-04-16 08:27:43 +0200

    Add files via upload

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -1,1 +1,7 @@
+// Get current year for copyright
+const currentYear = new Date().getFullYear();
+document.querySelector('footer p:first-of-type').innerHTML = `&copy; ${currentYear} 🏀 Enias Gramu 🏀 Zimbabwe.`;
 
+// Get last modified date
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
